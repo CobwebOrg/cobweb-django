08a18b64ca2bf8ef88644ada406a9452081aa3e1
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-07-18 13:24:22 -0700

    Very basic initial django site.
    So far it implements a preliminary version of the data model,
    which can be manipulated in django's autogenerated admin site.

---
+++ registry/admin.py
@@ -0,0 +1,74 @@
+from django.urls import reverse
+from django.contrib import admin
+
+from .models import Institution, Agent, Project, Seed, Claim, Holding
+
+
+class SeedInline(admin.TabularInline):
+    model = Seed
+    extra = 0
+
+    readonly_fields = [ 'url', 'description', 'changeform_link' ]
+    
+    def changeform_link(self, instance):
+        if instance.id:
+            # Replace "myapp" with the name of the app containing
+            # your Certificate model:
+            changeform_url = reverse(
+                'admin:registry_seed_change', args=(instance.id,)
+            )
+            return u'<a href="%s" target="_blank">Edit</a>' % changeform_url
+        return u''
+    changeform_link.allow_tags = True
+    changeform_link.short_description = ''   # omit column header
+    
+    fields = [ 'changeform_link', 'url', 'description', ]
+    
+class ClaimInline(admin.TabularInline):
+    model = Claim
+    extra = 0
+    
+    readonly_fields = [ 'created' ]
+    fields = [ 
+        'asserted_by', 
+        'seed', 
+        'institution', 
+        'start_date', 
+        'end_date', 
+        # frequency, 
+        'max_links', 
+        # host_limit, 
+        'time_limit', 
+        'document_limit', 
+        'data_limit', 
+        'robot_exclusion_override', 
+        # capture_software, 
+        'created', 
+        'deprecated', 
+    ]
+       
+class HoldingInline(admin.TabularInline):
+    model = Holding
+    extra = 0
+    
+    readonly_fields = [ 'created' ]
+    fields = [
+        'asserted_by', 
+        'seed', 
+        'institution', 
+        'created', 
+        'deprecated', 
+    ]
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ SeedInline ]
+
+class SeedAdmin(admin.ModelAdmin):
+    inlines = [ ClaimInline, HoldingInline ]
+
+
+admin.site.register(Institution)
+admin.site.register(Agent)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Seed, SeedAdmin)
---
+++ registry/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+
+    
+class Institution(models.Model):
+    name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+    # sector = ???
+    # type = ???
+    address = models.CharField('Address', max_length=1000)
+    # country = ???
+    # identifier = ???
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+    
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+   
+class Agent(models.Model):
+    # type = ???
+    name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description', null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    # identifier = ???
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+    
+    affiliation = models.ForeignKey(Institution, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField('Name', max_length=200)
+    description = models.TextField('Description')
+#    keywords ## Separate data type w/ many-to-many relationship??? ##
+#    descriptor
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+
+    established_by = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.name
+
+class Seed(models.Model):
+    url = models.URLField()
+    description = models.TextField('Description')
+    # keywords
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    nominated_by = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return self.url
+
+class Claim(models.Model):
+    # scope = ???
+    start_date = models.DateField('Starting Date')
+    end_date = models.DateField('Ending Date', null=True, blank=True)
+    # frequency = ???
+    max_links = models.IntegerField('Maximum Links', null=True, blank=True)
+    # host_limit = ???
+    time_limit = models.DurationField('Time Limit', null=True, blank=True)
+    document_limit = models.IntegerField('Document Limit', null=True, blank=True)
+    data_limit = models.IntegerField('Data Limit (GB)') # Allow fractional GB????
+    robot_exclusion_override = models.BooleanField('Override Robot Exclusion?', default=False)
+    # capture_software = ???
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+    
+    seed = models.ForeignKey(Seed, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    asserted_by = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return '{} claims {}'.format(self.institution, self.seed)
+
+class Holding(models.Model):
+    # scope = ???
+    created = models.DateTimeField('Date Created', auto_now_add=True)
+    deprecated = models.DateTimeField('Date Deprecated', null=True, blank=True)
+    
+    seed = models.ForeignKey(Seed, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    asserted_by = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    
+    def __str__(self):
+        return '{} has {}'.format(self.institution, self.seed)
