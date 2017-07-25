0683c5e1ee89e14a7e6fc5efe05863f86cba8019
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-07-25 16:29:23 -0700

    Generic views

--- registry/urls.py
+++ registry/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
+from django.contrib import admin
 from django.views import generic
 from . import models, views
 
-app_name = 'registry'
+# app_name = 'registry'
 urlpatterns = [
-    url(r'^$', views.ProjectIndexView.as_view(), name='project_index'),
-    # url(r'^project/(?P<pk>\d+)', views.ProjectDetailView.as_view(), name='project_detail'),
-    url(r'^(?P<model_name>\w+)/?$', views.object_list, name='object_list'),
-    url(r'^(?P<model_name>.+)/(?P<pk>.+)$', views.object_view, name='object_view'),
-    # url(r'^(?P<model_name>.+)/(?P<pk>.+)/save$', views.save_object, name='save_object'),
-    # url(r'^(?institution/P<id>[0-9]+)/$', , name='detail'),
+    # url(r'^$', views.projectindexview, name='project_index'),
+    url(r'^(?P<model_name>\w+)/?$', views.object_list_view, name='object_list'),
+    url(r'^(?P<model_name>\w+)/(?P<pk>.+)$/?', views.object_view, name='object_view'),
 ]
--- registry/views.py
+++ registry/views.py
@@ -6,47 +6,31 @@ from django.urls import reverse
 
 from . import models
 
-class ProjectIndexView(generic.ListView):
-    model = models.Project
-
-class ProjectDetailView(generic.DetailView):
-    model = models.Project
-
-def object_list(request, model_name):
-    try:
-        model = apps.get_model('registry', model_name)
-        return generic.ListView.as_view(model=model, template_name='project_list.html')(request)
-    except:
-        raise Http404("{model_name}: That's not a thing we have.".format(model_name=model_name))
+def object_list_view(request, model_name):
+    model = apps.get_model('registry', model_name)
+    request.model_name = model_name
+    request.verbose_name_plural = model._meta.verbose_name_plural
+    return generic.ListView.as_view(model=model, template_name='registry/object_list.html')(request)
 
 def object_view(request, model_name, pk):
     try:
         model = apps.get_model('registry', model_name)
     except:
-        raise Http404("{model_name}: no such class.".format(model_name=model_name))
+        raise Http404("{model_name}: no such model.".format(model_name=model_name))
     
-    fields = [field.name for field in model._meta.fields[1:] if field.editable]
+    as_view_arguments = dict(
+        model=model, 
+        template_name='registry/object_form.html',
+        fields=[field.name for field in model._meta.fields[1:] if field.editable],
+        success_url=reverse('object_list', kwargs={'model_name': model_name}),   
+    )
+    end_arguments = dict()
     if pk == "new":
-        return generic.CreateView.as_view(
-            model=model, 
-            template_name='registry/object_form.html',
-            fields=fields,
-            success_url=reverse('registry:object_list', kwargs={'model_name': model_name}),
-        )(request)
+        generic_view_object = generic.CreateView
     else:
-        try:
-            current_object = get_object_or_404(model, pk=pk)
-            return generic.UpdateView.as_view(
-                model=model,
-                template_name='registry/object_detail.html',
-                fields=fields,
-                success_url=reverse('registry:object_list', kwargs={'model_name': model_name}),
-            )(request, pk=pk)
-        except:
-            raise Http404("Can't find {model_name} {pk}.".format(model_name=model_name, pk=pk))
-
-# def save_object(request, model_name, pk):
-#     try:
-#         model = apps.get_model('registry', model_name)
-#         if pk == 'new':
-#             current_object = model()
+        generic_view_object = generic.UpdateView
+        end_arguments['pk'] = pk
+        
+    return generic_view_object.as_view(**as_view_arguments)(request, **end_arguments)
+        # except:
+            # raise Http404("Can't find {model_name} {pk}.".format(model_name=model_name, pk=pk))
