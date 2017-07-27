b7c56526a37ebe2ef5e34a2e1b425e42b4a63c22
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-07-27 12:31:13 -0700

    Views

--- registry/urls.py
+++ registry/urls.py
@@ -6,6 +6,9 @@ from . import models, views
 app_name = 'registry'
 urlpatterns = [
     # url(r'^$', views.projectindexview, name='project_index'),
-    url(r'^(?P<model_name>\w+)/?$', views.object_list_view, name='object_list'),
+    url(r'^project/(?P<pk>.+)$/?', views.ProjectDetailView.as_view(), name='project_detail'),
+    
+    # generic views using introspection -- replace these later
+    url(r'^(?P<model_name>\w+)/?', views.object_list_view, name='object_list'),
     url(r'^(?P<model_name>\w+)/(?P<pk>.+)$/?', views.object_view, name='object_view'),
 ]
--- registry/views.py
+++ registry/views.py
@@ -6,11 +6,31 @@ from django.urls import reverse
 
 from . import models
 
+def field_as_cell():
+    pass
+
+class ProjectDetailView(generic.DetailView):
+    model = models.Project
+
 def object_list_view(request, model_name):
     model = apps.get_model('registry', model_name)
     request.model_name = model_name
     request.verbose_name_plural = model._meta.verbose_name_plural
     return generic.ListView.as_view(model=model, template_name='registry/object_list.html')(request)
+
+def object_detail_view(request, model_name, pk):
+    try:
+        model = apps.get_model('registry', model_name)
+    except:
+        raise Http404("{model_name}: no such model.".format(model_name=model_name))
+    
+    as_view_arguments = dict(
+        model=model, 
+        template_name='registry/object_detail.html',
+        # fields=model.display_order,
+        # success_url=reverse('registry:object_list', kwargs={'model_name': model_name}),
+    )   
+    return generic.DetailView.as_view(**as_view_arguments)(request, pk=pk)
 
 def object_view(request, model_name, pk):
     try:
@@ -24,13 +44,13 @@ def object_view(request, model_name, pk):
         fields=[field.name for field in model._meta.fields[1:] if field.editable],
         success_url=reverse('registry:object_list', kwargs={'model_name': model_name}),   
     )
-    end_arguments = dict()
+    view_function_arguments = dict()
     if pk == "new":
         generic_view_object = generic.CreateView
     else:
         generic_view_object = generic.UpdateView
-        end_arguments['pk'] = pk
+        view_function_arguments['pk'] = pk
         
-    return generic_view_object.as_view(**as_view_arguments)(request, **end_arguments)
+    return generic_view_object.as_view(**as_view_arguments)(request, **view_function_arguments)
         # except:
             # raise Http404("Can't find {model_name} {pk}.".format(model_name=model_name, pk=pk))
