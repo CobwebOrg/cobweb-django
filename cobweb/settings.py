2fbe9d02dd490a900ab1bebbfb271878cfac30f8
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-07-28 16:46:29 -0700

    Use django-tables2 in views.

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'django.contrib.humanize',
+    'django_tables2',
     
     'registry.apps.RegistryConfig',
 ]
--- registry/views.py
+++ registry/views.py
@@ -5,7 +5,62 @@ from django.apps import apps
 from django.urls import reverse
 from django.template.loader import render_to_string
 
+from django_tables2 import Table, Column
+
 from . import models
+
+
+#
+#   Inline Views
+#
+        
+class InstitutionTable(Table):
+    class Meta:
+        model = models.Institution
+        
+class AgentTable(Table):
+    class Meta:
+        model = models.Agent
+        
+class ProjectTable(Table):
+    class Meta:
+        model = models.Project
+        attrs = {'class': 'table table-hover'}
+        fields = ('name', 'description')
+    
+    def render_name(self, record, value):
+        # print(">>> ", record, dir(record))
+        return '<a href="{url}">{name}</a>'.format(
+            name=value,
+            url=reverse('registry:project_detail', kwargs={'pk': record.pk})
+        )
+
+class SeedTable(Table):
+    class Meta:
+        model = models.Seed
+        attrs = {'class': 'table table-hover'}
+        fields = ('url', 'description', 'created', 'nominated_by')
+        
+class ClaimTable(Table):
+    class Meta:
+        model = models.Claim
+        
+class HoldingTable(Table):
+    class Meta:
+        model = models.Holding
+        
+table_class_key = {
+    models.Institution: InstitutionTable,
+    models.Agent: AgentTable,
+    models.Project: ProjectTable,
+    models.Seed: SeedTable,
+    models.Claim: ClaimTable,
+    models.Holding: HoldingTable,
+}
+
+#
+#   Page Views
+#
 
 class ProjectIndexView(generic.ListView):
     model = models.Project
@@ -17,6 +72,7 @@ def object_list_view(request, model_name):
     model = apps.get_model('registry', model_name)
     request.model_name = model_name
     request.verbose_name_plural = model._meta.verbose_name_plural
+    request.table = table_class_key[model](model.objects.all())
     return generic.ListView.as_view(model=model, template_name='registry/object_list.html')(request)
 
 def object_view(request, model_name, pk):
