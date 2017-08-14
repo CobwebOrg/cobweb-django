9ecfbe761f6078205e0e9c02b075503642256452
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-08-14 08:51:32 -0700

    django_extensions graph of data model

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     'django.contrib.humanize',
     'django_tables2',
+    'django_extensions',
     
     'registry.apps.RegistryConfig',
 ]
--- start-development.sh
+++ start-development.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+python3 manage.py graph_models registry > 'dataModel.dot'
 python3 manage.py runserver 0.0.0.0:8000
