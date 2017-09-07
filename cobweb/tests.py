c28ecfe7481f03f40a6b4391234ab1f8cee578b4
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-09-07 12:46:19 -0700

    Show each resource once in ProjectDetailView, regardless of number of nominations.

--- cobweb/tests.py
+++ cobweb/tests.py
@@ -198,7 +198,8 @@ class DetailViewTestsMixin(ViewTestsMixin):
 
     def test_included_fields(self):
         for field in self.fields:
-            self.assertContains(self.test_response, getattr(self.test_instance, field))
+            # ipdb.set_trace()
+            self.assertContains(self.test_response, getattr(self.test_instance, field), html=True)
 
     def test_update_link(self):
         pass
@@ -269,18 +270,42 @@ class ProjectIndexViewTests(TestCase):
         self.assertContains(self.client.get('/projects/'), 'Create new project')
         self.assertContains(self.client.get('/projects/'), reverse('project_create'))
 
-class ProjectDetailViewTests(TestCase):
+class ProjectDetailViewTests(DetailViewTestsMixin, TestCase):
 
     def setUp(self):
-        self.agent = get_agent()
-        self.project = get_project(name="Boring Project", established_by=self.agent)
-        self.response = self.client.get(self.project.get_absolute_url())
+        self.test_instance = get_project(description = "Just a test project.")
+        self.fields = [ 'name', 'description' ]
+        self.templates = [ 'base.html', 'project_detail.html' ]
 
-    def test_detail_view(self):
-        self.assertTemplateUsed(self.response, 'base.html')
-        self.assertTemplateUsed(self.response, 'project_detail.html')
-        self.assertEqual(self.response.status_code, 200)
-        self.assertContains(self.response, 'Boring Project')
+        # Add some nominations
+        self.user_nominations = {
+            'facebook.com': ('user1', 'user3'),
+            'linkedin.com': ('user2', 'user3'),
+            'myspace.com': ('user3'),
+            'twitter.com': ('user1', 'user2', 'user3'),
+            'ucla.edu': ('user1', 'user3'),
+        }
+        for url, users in self.user_nominations.items():
+            for username in users:
+                get_nomination(
+                    project=self.test_instance,
+                    nominated_by=get_agent(user=get_user(username=username)),
+                    resource=get_resource(root_url=url),
+                )
+
+
+        self.client.logout()
+        self.test_response = self.client.get(self.test_instance.get_absolute_url())
+
+    def test_each_nominated_resource_listed_only_once(self):
+        """Each nominated resource should be listed on the Project Detail page.
+        If it was nominated by multiple users, it should appear only once.
+
+        As currently written, this test will also fail if the resource url is 
+        included in a link, which shouldn't happen."""
+        
+        for url, users in self.user_nominations.items():
+            self.assertContains(self.test_response, url, count=1)
 
     def test_edit_project_link(self):
         """An 'edit project' link should be shown if logged-in user is authorized,
@@ -289,30 +314,29 @@ class ProjectDetailViewTests(TestCase):
         pass
 
     def test_new_nomination_link(self):
-        """A 'new project' link should be shown if logged-in user is authorized,
+        """A 'nominate' link should be shown if logged-in user is authorized,
         otherwise hidden."""
 
         self.client.logout()
-        response = self.client.get(self.project.get_absolute_url())
+        response = self.client.get(self.test_instance.get_absolute_url())
         self.assertNotContains(response, 'Add a nomination')
-        self.assertNotContains(response, reverse('nominate', kwargs={'project_id': self.project.pk}))
+        self.assertNotContains(response, reverse('nominate', kwargs={'project_id': self.test_instance.pk}))
 
-        self.client.force_login(self.agent.user)
-        response = self.client.get(self.project.get_absolute_url())
+        self.client.force_login(self.test_instance.established_by.user)
+        response = self.client.get(self.test_instance.get_absolute_url())
         self.assertContains(response, 'Add a nomination')
-        self.assertContains(response, reverse('nominate', kwargs={'project_id': self.project.pk}))
+        self.assertContains(response, reverse('nominate', kwargs={'project_id': self.test_instance.pk}))
 
-    # def test_seed_list(self):
-    #     all_seeds = Seed.objects.all()
-    #     project_seeds = self.project.seed_set.all()
-    #     for seed in all_seeds:
-    #         if seed in project_seeds:
-    #             self.assertContains(self.response, seed.url)
-    #         else:
-    #             self.assertNotContains(self.response, seed.url)
-    #
-    # def test_no_link_to_seed_url(self):
-    #     self.assertNotContains(self.response, 'href="http://nytimes.com"')
+    def test_username_not_agent_str(self):
+        """'Created by' lists string representation of User, not Agent."""
+        self.assertContains(
+            self.test_response, 
+            self.test_instance.established_by.user.get_full_name()
+        )
+        self.assertNotContains(
+            self.test_response, 
+            str(self.test_instance.established_by)
+        )
 
 class ProjectCreateViewTests(TestCase):
 
