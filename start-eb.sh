241c00e1ffd3c8637258946be35628f62b0d1c51
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-09-13 17:58:04 -0700

    run ait harvester in aws

--- start-eb.sh
+++ start-eb.sh
@@ -3,6 +3,7 @@ set -e
 
 python3 manage.py migrate --noinput
 # python3 manage.py collectstatic --noinput
-python3 manage.py archive-it&
+python3 manage.py archive-it
+python3 manage.py ait-resources.py &
 
 exec uwsgi uwsgi.ini
