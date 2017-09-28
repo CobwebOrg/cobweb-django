2e0e97469e7f271534e3e90f9b455ef2a37b0ca4
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-09-28 10:01:18 -0600

    Current data model diagrams

--- start-test.sh
+++ start-test.sh
@@ -3,5 +3,7 @@
 
 coverage run manage.py test
 coverage html
-python3 manage.py graph_models core projects archives webresources datasources -X AbstractUser -g -o datamodel.png
+python3 manage.py graph_models core projects archives webresources datasources metadata -X AbstractUser -g -o datamodel.png
 python3 manage.py graph_models -a -g -o datamodel-full.png
+python3 manage.py graph_models core projects archives datasources -X Resource,Metadatum -g -o datamodel-simple.png
+python3 manage.py graph_models metadata -g -o datamodel-metadata.png
