33d420a22b83d506bef496a562ca0cb2bfb49fd3
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-10-11 07:44:55 -0700

    Added aws staging env to ALLOWED_HOSTS

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -27,7 +27,11 @@ DEBUG = True
 
 # INTERNAL_IPS = ['127.0.0.1']
 INTERNAL_IPS = ['72.18.0.1']
-ALLOWED_HOSTS =  ['cobweb-dev.xvkpjfy2kp.us-west-2.elasticbeanstalk.com', 'localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS =  [
+    'cobweb-dev.xvkpjfy2kp.us-west-2.elasticbeanstalk.com',
+    'cobweb-dev-staging.xvkpjfy2kp.us-west-2.elasticbeanstalk.com',
+     'localhost', '127.0.0.1', '[::1]'
+]
 
 
 # Application definition
