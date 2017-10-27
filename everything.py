772b2e64e8469e7bcb64af827833e8451b714373
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-10-26 22:24:27 -0700

    Thought I fixed everything.py

--- everything.py
+++ everything.py
@@ -6,19 +6,19 @@
 
 from archives.models import *
 from archives.views import *
-from archives.search_indexes import *
+# from archives.search_indexes import *
 from archives.tests import *
 
 from core.models import *
 from core.views import *
 from core.forms import *
-from core.search_indexes import *
+# from core.search_indexes import *
 from core.tests import *
 
 from projects.models import *
 from projects.views import *
 from projects.forms import *
-from projects.search_indexes import *
+# from projects.search_indexes import *
 from projects.tests import *
 
 from datasources.models import *
