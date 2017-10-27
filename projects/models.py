6d06df2d0b041b91d7ebc499348efd44cc8c1dc9
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-10-26 22:05:36 -0700

    Minor formatting

--- projects/models.py
+++ projects/models.py
@@ -25,8 +25,8 @@ class Project(CobwebMetadataMixin, models.Model):
     nominators = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True,
         related_name='projects_nominating')
 
-    nominator_blacklist = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True,
-        related_name='projects_blacklisted')
+    nominator_blacklist = models.ManyToManyField(settings.AUTH_USER_MODEL, 
+        blank=True, related_name='projects_blacklisted')
 
     class STATUS(Enum):
         active = ('a', 'Active')
