23efb7d29bc236b979c0e833d936e4c03109fe4c
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-10-28 14:09:24 -0700

    Added some stuff to .gitignore

--- start-test.sh
+++ start-test.sh
@@ -2,7 +2,7 @@
 
 
 coverage run manage.py test
-coverage html
-python3 manage.py graph_models core projects archives webresources datasources -X AbstractUser -g -o datamodel.png
-python3 manage.py graph_models -a -g -o datamodel-full.png
-python3 manage.py graph_models core projects archives datasources -X Resource -g -o datamodel-simple.png
+coverage html -d code_analysis/htmlcov
+python3 manage.py graph_models core projects archives webresources datasources -X AbstractUser -g -o code_analysis/datamodel.png
+python3 manage.py graph_models -a -g -o code_analysis/datamodel-full.png
+python3 manage.py graph_models core projects archives datasources -X Resource -g -o code_analysis/datamodel-simple.png
