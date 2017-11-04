6c17fcb7adaf11c6bb7f01f7c51c6e54fd7aa220
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-04 08:38:20 -0700

    Don't load django_debug_toolbar on AWS, disable all toolbars by default on dev machine.
    Necessary because some panels (SQL, others unknown) were adding 1–2
    seconds to page response time.

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -184,29 +184,31 @@ LOGIN_REDIRECT_URL = '/'
 import sys
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
-if not TESTING:
+# Use debug toolbar on local dev machine only
+if 'RDS_HOSTNAME' not in os.environ and not TESTING:
     MIDDLEWARE += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar', )
 
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-]
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ]
 
-def show_toolbar(request):
-    return True
+    def show_toolbar(request):
+        return True
 
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': 'cobweb.settings.show_toolbar',
-    'INTERCEPT_REDIRECTS': False,
-}
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': 'cobweb.settings.show_toolbar',
+        'INTERCEPT_REDIRECTS': False,
+        'DISABLE_PANELS': DEBUG_TOOLBAR_PANELS,
+    }
