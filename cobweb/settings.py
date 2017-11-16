ff092438ef782b6a0d92ac24f35020d614551317
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-16 06:57:29 -0800

    New, protected SECRET_KEY

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0+i0#su8em*jpun1hnys3ll-6kedb=-rq%2^=8&o8r8oacxz4g'
+SECRET_KEY = os.environ['COBWEB_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
