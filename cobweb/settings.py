eefeb2f86246dc1e12c4b4780afea60dfedfa808
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-16 07:51:24 -0800

    (Hopefully) better way to protect SECRET_KEY

--- cobweb/settings.py
+++ cobweb/settings.py
@@ -21,7 +21,29 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['COBWEB_DJANGO_SECRET_KEY']
+# cf. https://gist.github.com/ndarville/3452907
+try:
+    SECRET_KEY
+except NameError:
+    SECRET_FILE = os.path.join(BASE_DIR, 'secret.txt')
+    try:
+        SECRET_KEY = open(SECRET_FILE).read().strip()
+    except IOError:
+        try:
+            import random
+            SECRET_KEY = ''.join([
+                random.SystemRandom().choice(
+                    'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+                )
+                for i in range(50)
+            ])
+            with open(SECRET_FILE, 'w') as secret:
+                secret.write(SECRET_KEY)
+                secret.close()
+        except IOError:
+            Exception('Please create a %s file with random characters \
+            to generate your secret key!' % SECRET_FILE)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
