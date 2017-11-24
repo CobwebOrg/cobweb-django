4b6bd5969597c2813eb21d863514e149141f1a9d
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-24 11:47:43 -0800

    AWS EB Multicontainer

--- ebcreate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-eb create cobweb-dev-staging \
-    --database \
-    --database.engine postgres \
-    --database.instance db.t2.micro \
-    --database.size 5 \
-    --database.username cobweb \
-    
