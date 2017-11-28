afdcafe8318730c879564204b6a035866847629f
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-28 15:24:20 -0800

    Moved Solr and Postgres data from mounted directory to Docker volumes.

--- start-eb.sh
+++ start-eb.sh
@@ -3,6 +3,7 @@ set -e
 
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
-nohup nice python3 manage.py archive-it &
+
+python3 manage.py build_solr_schema -c data/solr/cobweb/conf -r cobweb
 
 exec uwsgi uwsgi.ini
