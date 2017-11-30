d8f8b9a3038cf689fdf6b3e5acfdb7ef46c57c29
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-11-30 12:41:23 -0800

    Docker environment work - should run on EC2

--- start-development.sh
+++ start-development.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 pip3 freeze > requirements-frozen.txt
 python3 manage.py runserver 0.0.0.0:8000
--- start-eb.sh
+++ start-eb.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+pip install -r requirements-frozen.txt
+
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
 python3 manage.py build_solr_schema -c data/solr/cobweb/conf -r cobweb
+python3 manage.py update_index
 
 exec uwsgi uwsgi.ini
