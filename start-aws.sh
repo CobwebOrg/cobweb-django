c098a5e9b58d725f82c59899d586714500c63bb4
Andrew Wallace <andrewwallace@library.ucla.edu>
2017-12-13 16:44:22 -0700

    moved solrdata docker volume out of /code

--- start-aws.sh
+++ start-aws.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+pip install -r requirements-frozen.txt
 
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
-python3 manage.py build_solr_schema -c data/solr/cobweb/conf -r cobweb
+python3 manage.py build_solr_schema -c /solrdata/cobweb/conf -r cobweb
 python3 manage.py update_index
 
 exec uwsgi uwsgi.ini
