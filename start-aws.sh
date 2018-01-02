a95778fcb64fa6de7f89e4f70136e6b09e53009c
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-01-01 16:17:10 -0800

    Correct solr_data path in start-aws.sh

--- start-aws.sh
+++ start-aws.sh
@@ -5,7 +5,7 @@ pip install -r requirements-frozen.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
-python3 manage.py build_solr_schema -c /solrdata/cobweb/conf -r cobweb
+python3 manage.py build_solr_schema -c /solr_data/cobweb/conf -r cobweb
 python3 manage.py update_index
 
 exec uwsgi uwsgi.ini
