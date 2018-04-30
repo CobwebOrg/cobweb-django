d6c6728a3dd8523ee128e94e256ddf23e0ba5329
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-04-30 13:32:46 -0700

    chore: fixed some docker things

--- start-development.sh
+++ start-development.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-DJANGO_SETTINGS_MODULE=cobweb.settings.debug python3 manage.py runserver 0.0.0.0:8000
+pipenv install --dev
+pipenv run python3 manage.py runserver 0.0.0.0:8000
