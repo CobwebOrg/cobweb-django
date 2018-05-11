e54757fc57abfa301d74d39fab07688914015088
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-05-11 13:19:51 -0700

    feat: header work

--- cobweb/settings/production.py
+++ cobweb/settings/production.py
@@ -203,7 +203,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "assets"),
 ]
 
