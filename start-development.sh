d46126fb1b6a286f2dc96e615b1b57ecce4b07c7
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-05-14 15:37:46 -0700

    refactor: in dev container, pipenv install to system, not virtualenv

--- start-development.sh
+++ start-development.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-pipenv install --dev
-pipenv run python3 manage.py runserver 0.0.0.0:8000
+pipenv install --dev --system --ignore-pipfile
+python3 manage.py runserver 0.0.0.0:8000
