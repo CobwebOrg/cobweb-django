02a4f018de1b062e59478b44884b62b957c602dd
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-06-01 17:11:04 -0700

    env: solr management

--- scripts/solr_update.sh
+++ scripts/solr_update.sh
@@ -1,4 +1,5 @@
-docker-compose run web pipenv run python3 manage.py build_solr_schema --configure-directory solr_conf/
+pipenv run python3 manage.py build_solr_schema --configure-directory solr_conf/
 docker-compose build solr
 docker-compose restart solr
-docker-compose run web pipenv run python3 manage.py rebuild_index
+pipenv run python3 manage.py rebuild_index --noinput
+
