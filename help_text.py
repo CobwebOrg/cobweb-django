2e6391ae625a8bb1c277fa5a18cf1cbb059aeb1b
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-09-28 09:36:32 -0700

    feat: easily-editable help_text.py

---
+++ help_text.py
@@ -0,0 +1,75 @@
+# TODO: help text in crispy field template
+
+# TODO: help text in model
+CLAIM_HELD = """
+    Check here to indicate that your organization has both made this claim to
+    capture the nominated URL and that you have successfully captured it.
+    """
+
+# TODO: column headers in lots of tables
+N_CLAIMED = """
+    Number of resource URLs nominated for this project that have been claimed
+    for capture, but not yet captured, by collecting organizations.
+    """
+
+# TODO: most table column headers
+N_HELD = """
+    Number of resource URLs nominated for this project that have been reported
+    by collecting organizations as having been captured.
+    """
+
+# TODO: most table column headers
+N_UNCLAIMED = """
+    Number of resource URLs nominated for this project that have not been
+    claimed for capture by any collecting organization.
+    """
+
+# TODO: help text in model
+NOMINATION_RATIONALE = """
+    Describe why you think this particular resource should be included with
+    this project.
+    """
+
+# TODO: help text in model
+ORGANIZATION_PROFILE = "Choose a Cobweb URL for your organization."
+
+# TODO: help text in model
+PROJECT_ADMINS = """
+    Enter names of registered Cobweb users who can edit and manage project
+    information. Names can include more than one word, separated by spaces and
+    ending with a hard return for entry. For projects with a restricted
+    nomination policy, administrators are automatically included among selected
+    users.
+    """
+
+# TODO: model help text
+PROJECT_DESCRIPTION = """
+    Describe the intended content (such as a theme, topic, or event it is
+    intended to document) and goals of this project.
+    """
+
+# TODO: model help text
+PROJECT_COLLECTING_SCOPE = """
+    As applicable, describe the scope and approach that this project is
+    intended to take (such as collecting for defined time frame).
+    """
+
+# TODO: help text in model
+PROJECT_NOMINATORS = """
+    Enter names of selected Cobweb users who can nominate to projects with a
+    restricted nomination policy. Names can include more than one word,
+    separated by spaces and ending with a hard return for entry.
+    """
+
+# TODO: model help text
+PROJECT_NOMINATOR_BLACKLIST = """
+    Enter names of Cobweb users who cannot nominate to this project. Names can
+    include more than one word, separated by spaces and ending with a hard
+    return for entry.
+    """
+
+# TODO: help text in many models (search for tag fields)
+TAGS = """
+    Enter or choose brief descriptive terms. Descriptive terms can include more
+    than one word, separated by spaces and ending with a hard return for entry.
+    """
