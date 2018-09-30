fd2ffb62c2f40b04859967d251714c221fedd1ac
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-09-30 15:10:32 -0700

    tweak: smaller textareas

--- assets/js/cobweb.js
+++ assets/js/cobweb.js
@@ -10,15 +10,6 @@ import solid from '@fortawesome/fontawesome-free-solid';
 
 fontawesome.library.add(solid);
 
-// // Make TextAreas auto-sizing
-
-// $('textarea').each(function () {
-//   this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-// }).on('input', function () {
-//   this.style.height = 'auto';
-//   this.style.height = (this.scrollHeight) + 'px';
-// });
-
 // Enable tooltips and popovers
 
 $(function () {
