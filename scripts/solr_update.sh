de2f1e4232642a6626b4607178548f882de9973b
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-10-19 10:37:03 -0700

    fix: update solr-test container in scripts/solr_update

--- scripts/solr_update.sh
+++ scripts/solr_update.sh
@@ -1,4 +1,4 @@
 docker-compose run web python3 manage.py build_solr_schema --configure-directory solr_conf/
-docker-compose up --build --force-recreate --detach solr 
+docker-compose up --build --force-recreate --detach solr solr-test
 sleep 15
 docker-compose run web python3 manage.py rebuild_index --noinput
