1955a39549362fa55b1b904576c62f47a62cdafe
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-10-19 10:38:08 -0700

    fix: update index of projects before resources on deploy

--- scripts/start-worker.sh
+++ scripts/start-worker.sh
@@ -2,6 +2,7 @@
 
 python3 /code/manage.py migrate --noinput
 sleep 1m
-python3 /code/manage.py update_index
+python3 /code/manage.py update_index -k 2 projects
+python3 /code/manage.py update_index -k 2 core
 # python3 /code/manage.py archive-it
 
