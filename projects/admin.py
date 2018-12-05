bf66826b27673b67b23e967daeb0acd203b67362
Andrew Wallace <andrewwallace@library.ucla.edu>
2018-12-05 12:01:57 -0800

    admin: autocomplete_fields for NominationAdmin, ClaimAdmin

--- projects/admin.py
+++ projects/admin.py
@@ -8,16 +8,35 @@ from projects.admin_inlines import NominationInline
 
 @admin.register(Project)
 class ProjectAdmin(VersionAdmin):
-    autocomplete_fields = ['administrators', 'nominator_orgs', 'nominators',
-                           'nominator_blacklist', 'tags', 'subject_headings']
+    autocomplete_fields = [
+        'administrators',
+        'nominator_orgs',
+        'nominators',
+        'nominator_blacklist',
+        'tags',
+        'subject_headings',
+    ]
     # inlines = [NominationInline, NoteInline]
 
 
 @admin.register(Nomination)
 class NominationAdmin(VersionAdmin):
+    autocomplete_fields = [
+        'resource',
+        'project',
+        'language',
+        'tags',
+        'subject_headings',
+    ]
     inlines = [NoteInline]
 
 
 @admin.register(Claim)
 class ClaimAdmin(VersionAdmin):
+    autocomplete_fields = [
+        'nomination',
+        'organization',
+        'imported_record',
+        'crawl_scope',
+    ]
     inlines = [NoteInline]
